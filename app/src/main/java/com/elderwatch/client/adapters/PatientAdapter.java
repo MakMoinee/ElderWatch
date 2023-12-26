a641dbc479e20f40960f2aead4a29800430210e0
MakMoinee <makmoinee@gmail.com>
2023-12-26 21:23:24 +0800

    update code

---
+++ app/src/main/java/com/elderwatch/client/adapters/PatientAdapter.java
@@ -0,0 +1,51 @@
+package com.elderwatch.client.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.elderwatch.client.R;
+import com.elderwatch.client.interfaces.PatientListener;
+import com.elderwatch.client.models.Patients;
+
+import java.util.List;
+
+public class PatientAdapter extends RecyclerView.Adapter<PatientAdapter.ViewHolder> {
+
+    Context mContext;
+    List<Patients> patientsList;
+    PatientListener listener;
+
+    public PatientAdapter(Context mContext, List<Patients> patientsList, PatientListener listener) {
+        this.mContext = mContext;
+        this.patientsList = patientsList;
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public PatientAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View mView = LayoutInflater.from(mContext).inflate(R.layout.item_patient,parent,false);
+        return new ViewHolder(mView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PatientAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return patientsList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
---
+++ app/src/main/java/com/elderwatch/client/ui/patients/PatientFragment.java
@@ -0,0 +1,103 @@
+package com.elderwatch.client.ui.patients;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.elderwatch.client.adapters.PatientAdapter;
+import com.elderwatch.client.databinding.FragmentPatientBinding;
+import com.elderwatch.client.interfaces.PatientListener;
+import com.elderwatch.client.models.Patients;
+import com.elderwatch.client.services.FSRequest;
+import com.github.MakMoinee.library.interfaces.FirestoreListener;
+import com.github.MakMoinee.library.models.FirestoreRequestBody;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PatientFragment extends Fragment {
+
+    FragmentPatientBinding binding;
+    FSRequest request;
+
+    List<Patients> patientsList = new ArrayList<>();
+
+    PatientAdapter adapter;
+    ProgressDialog pDialog;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentPatientBinding.inflate(LayoutInflater.from(requireContext()),container,false);
+        request = new FSRequest();
+        pDialog = new ProgressDialog(requireContext());
+        pDialog.setMessage("Loading ...");
+        pDialog.setCancelable(false);
+        loadData();
+        setListeners();
+        return binding.getRoot();
+    }
+
+    private void loadData() {
+        pDialog.show();
+        patientsList = new ArrayList<>();
+        FirestoreRequestBody body = new FirestoreRequestBody.FirestoreRequestBodyBuilder()
+                .setCollectionName(FSRequest.PATIENTS_COLLECTION)
+                .build();
+
+        request.findAll(body, new FirestoreListener() {
+            @Override
+            public <T> void onSuccess(T any) {
+                pDialog.dismiss();
+                if(any instanceof QuerySnapshot snapshots){
+                    if(!snapshots.isEmpty()){
+                        for(DocumentSnapshot documentSnapshot: snapshots){
+                            if(documentSnapshot.exists()){
+                                Patients patients = documentSnapshot.toObject(Patients.class);
+                                if(patients!=null){
+                                    patients.setPatientID(documentSnapshot.getId());
+                                    patientsList.add(patients);
+                                }
+                            }
+                        }
+                    }
+
+                    if(patientsList.size()>0){
+                        adapter = new PatientAdapter(requireContext(), patientsList, new PatientListener() {
+                            @Override
+                            public void onClickListener() {
+
+                            }
+                        });
+                        binding.recycler.setLayoutManager(new LinearLayoutManager(requireContext()));
+                        binding.recycler.setAdapter(adapter);
+                    }
+                }
+            }
+
+            @Override
+            public void onError(Error error) {
+                pDialog.dismiss();
+                Toast.makeText(requireContext(), "There are no patients added yet", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void setListeners() {
+        binding.btnAddPatient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
